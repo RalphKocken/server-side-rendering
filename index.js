3352b6611a3d78bf40379009d7df7aaa46c59a04
RalphKocken <106448490+RalphKocken@users.noreply.github.com>
2023-03-12 17:32:07 +0100

    update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express'
-
 const url = 'https://api.buurtcampus-oost.fdnd.nl/api/v1'
+
 
 // Maak een nieuwe express app
 const app = express()
@@ -28,19 +28,7 @@ app.get('/plantpage', (request, response) => {
   })
 })
 
-/**
- * Wraps the fetch api and returns the response body parsed through json
- * @param {*} url the api endpoint to address
- * @returns the json response from the api endpoint
- */
-
-async function fetchJson(url) {
-  return await fetch(url)
-    .then((response) => response.json())
-    .catch((error) => error)
-}
-
-// // Stel het poortnummer in waar express op gaat luisteren
+// Stel het poortnummer in waar express op gaat luisteren
 app.set('port', process.env.PORT || 8000)
 
 // Start express op, haal het ingestelde poortnummer op
@@ -48,3 +36,14 @@ app.listen(app.get('port'), function () {
   // Toon een bericht in de console en geef het poortnummer door
   console.log(`Application started on http://localhost:${app.get('port')}`)
 })
+
+/**
+ * Wraps the fetch api and returns the response body parsed through json
+ * @param {*} url the api endpoint to address
+ * @returns the json response from the api endpoint
+ */
+async function fetchJson(url) {
+  return await fetch(url)
+    .then((response) => response.json())
+    .catch((error) => error)
+}
